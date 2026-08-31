2e6394d62c361aa15ad4a0880b4ef92608338850
FrezzDEV <102240197+FrezzDEV@users.noreply.github.com>
2026-08-31 17:44:26 +0300

    chore: add feature-first main navigation shell

---
+++ lib/features/navigation/presentation/main_shell.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+import '../../history/presentation/screens/history_screen.dart';
+import '../../home/presentation/screens/home_screen.dart';
+import '../../settings/presentation/screens/settings_screen.dart';
+
+class MainShell extends StatefulWidget {
+  const MainShell({super.key});
+
+  @override
+  State<MainShell> createState() => _MainShellState();
+}
+
+class _MainShellState extends State<MainShell> {
+  int _currentIndex = 0;
+
+  static const _pages = <Widget>[
+    HomeScreen(),
+    HistoryScreen(),
+    SettingsScreen(),
+  ];
+
+  void _onDestinationSelected(int index) {
+    setState(() => _currentIndex = index);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: _currentIndex,
+        children: _pages,
+      ),
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: _currentIndex,
+        onDestinationSelected: _onDestinationSelected,
+        destinations: const [
+          NavigationDestination(
+            icon: Icon(Icons.home_outlined),
+            selectedIcon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.history_outlined),
+            selectedIcon: Icon(Icons.history),
+            label: 'History',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.settings_outlined),
+            selectedIcon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
+      ),
+    );
+  }
+}
